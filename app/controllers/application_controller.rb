21c73eb8611f608ed56f5a14b91f3f90d4ea7c54
naofumi-fujii <naofumi.fujii@gmail.com>
2016-02-24 22:16:12 +0900

    rubocop autofix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  before_filter :set_locale, :set_current_user
+  before_action :set_locale, :set_current_user
 
   # override- set locale to all url
   def default_url_options(options = {})
